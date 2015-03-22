7efaad59e0408d2a5c0e70a8698fa032335bbcc0
mitchabaza <mitch.abaza@gmail.com>
2015-03-21 20:31:06 -0500

    s

---
+++ ToDo.Client/app/js/__tests__/App-react-test.js
@@ -0,0 +1,30 @@
+﻿jest.dontMock("../components/App.jsx");
+jest.mock("../Stores/ToDoStore");
+jest.dontMock("underscore");
+jest.dontMock("events");
+
+ describe('Store', function () {
+    
+    //jest.dontMock("../stores/Store.js");
+    //jest.dontMock("underscore");
+
+
+     
+     
+    it('gets initial state from Store', function () {
+          var App = require("../components/App.jsx");
+        var React = require('react/addons');
+        var TestUtils = React.addons.TestUtils;
+        //console.log("mock" + Store.getAll());
+
+        var output= TestUtils.renderIntoDocument(<App/>);
+        var comp = TestUtils.scryRenderedDOMComponentsWithTag(output,"section");
+//        console.log("output" + comp.getDOMNode().innerHTML);
+        console.log(comp.length);
+
+    });
+    
+   
+    
+ 
+});
